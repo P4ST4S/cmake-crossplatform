e58f5b2367daddf6c6b778c4775ced0e8c4a058a
P4ST4S <antoine.rsopars@epitech.eu>
2023-09-18 15:04:42 +0100

    yeaah

--- cmake-build/CMakeFiles/calculator.dir/addition.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/calculator.dir/addition.cpp.o: \
- /home/pastas/test/cmake-tuto/addition.cpp /usr/include/stdc-predef.h
--- cmake-build/CMakeFiles/calculator.dir/division.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/calculator.dir/division.cpp.o: \
- /home/pastas/test/cmake-tuto/division.cpp /usr/include/stdc-predef.h
